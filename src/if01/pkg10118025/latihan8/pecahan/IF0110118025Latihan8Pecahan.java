5fe603729cd01e3fe290c8526e843292aa001068
rizki2704 <55817452+rizki2704@users.noreply.github.com>
2019-09-26 08:07:43 +0700

    Add files via upload

---
+++ src/if01/pkg10118025/latihan8/pecahan/IF0110118025Latihan8Pecahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pkg10118025.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * Nama     : Rizki Restu Illahi
+ * Kelas    : IF01
+ * NIM      : 10118025
+ */
+public class IF0110118025Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        
+        System.out.println("Float : " +f);
+        System.out.println("Double : " +d);
+                
+    }
+    
+}
